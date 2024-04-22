f5e85cbab04ae300e85727f08c59007499c3c115
FelipePinheiro <fsp12371@git.com>
2024-04-22 18:11:02 -0300

    Página atualizada mas com erro

--- src/app/Alunos/page.tsx
+++ src/app/Alunos/page.tsx
@@ -3,24 +3,18 @@ import NavBar from "@/components/NavBar";
 import { Apple, ChevronDown, Plus } from "lucide-react";
 import { CategoriaItem } from "./CategoriaItem";
 import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import { getAlunos } from "../actions/Alunos/get";
 
+interface Aluno {
+  id: number,
+  nome: string,
+  turma:string
+}
 
-export default function Aluno() {
+export default async function Aluno() {
 
-  const categorias = [
-    {
-      id: 1,
-      nome: "Felipe"
-    },
-    {
-      id: 2,
-      nome: "Vinão"
-    },
-    {
-      id: 3,
-      nome: "Tavão"
-    }
-  ]
+  const alunos: Aluno[] = await getAlunos()
 
   return (
     <main className="flex min-h-screen flex-col items-center">
@@ -29,12 +23,14 @@ export default function Aluno() {
       <section className="flex flex-col gap-6 mt-6 p-6 bg-white rounded min-w-[500px]">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold">Alunos cadastrados</h2>
-          <Button color="secondary" startContent={<Plus size={18}/>}>
-            novo aluno
-          </Button>
+          <Link href="/Alunos/new">
+            <Button color="secondary" startContent={<Plus size={18}/>}>
+              novo aluno
+            </Button>
+          </Link>
         </div>
 
-        {categorias.map(categoria => <CategoriaItem categoria={categoria} />)}
+        {alunos.map(categoria => <CategoriaItem categoria={categoria} />)}
 
 
         
